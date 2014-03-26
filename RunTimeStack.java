70989552e330ab862b82abb0ddac6b7676010abc
Rotoscope <ticktick58@gmail.com>
2014-03-26 00:30:12 -0700

    Update RunTimeStack.java

--- RunTimeStack.java
+++ RunTimeStack.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class RunTimeStack {
     Stack framePointers;
-    Vector<ByteCode> runStack;
+    Vector runStack;
     
     public RunTimeStack() {
 	framePointers = new Stack();
